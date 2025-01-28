3f2b20b9a564b8a15ffaf1fec75b002ab2dc4dcb
AbdielPerezB <abdiel2012p@gmail.com>
2025-01-28 01:35:04 -0600

    initial commit

---
+++ src/data/invoice.js
@@ -0,0 +1,22 @@
+export const invoice = {
+    id: 1,
+    name: "Abdiel",
+    client: {
+        name: "Pepe",
+        lasName: "Doe",
+        address: {
+            country: "USA",
+            city: "Boston",
+            street: "Amalucan",
+            number: 2
+        },
+        company: {
+            name: "Negocios chuecos Inc.",
+            fiscalNumber: 1233445
+        },
+        items: [
+            
+        ]
+    }
+
+}
